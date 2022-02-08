71a97520276d9cad46efb18f3a21ed04b0e67850
Danya <dannyson5@bk.ru>
2022-02-09 01:55:37 +0300

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
-import { createStore } from 'vuex'
+import {createStore} from "vuex";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        likes: 2,
+        isAuth: false,
+    },
+    getters: {
+        doubleLikes(state) {
+            return state.likes * 2;
+        }
+    },
+    mutations: {
+        incrementLikes() {
+            this.state.likes += 1;
+        },
+        decrementLikes() {
+            this.state.likes -= 1;
+        }
+    },
+    actions: {
+
+    },
+    modules: {
+
+    }
+});
